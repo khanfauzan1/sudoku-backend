08f5f192a99d8f322a1198fa619c1e4cff4cc6da
FAUZAN AHMAD <110845300+khanfauzan1@users.noreply.github.com>
2024-07-29 18:51:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const sudokuSolver = require('./sudokuSolver');
 // app.use(cors());
 app.use(cors({ origin: "http://localhost:3000",methods:["POST","GET","PUT","DELETE"], credentials: true }));
 app.use(express.json());
-
+app.get("/",(req,res)=>{
+  res.send("hello");
+})
 app.post('/api/solve', (req, res) => {
   const { puzzle } = req.body;
   const solution = sudokuSolver.solve(puzzle);
